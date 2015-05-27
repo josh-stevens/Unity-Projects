54708234b93ac7e042bf7d38e3ec30b62b674494
josh-stevens <jcstevens12@outlook.com>
2015-05-27 18:41:07 -0500

    Added BrickBreaker project
    This is 2 days of work on this project. So far it has:
    
    - a Start screen with cool free fonts, and a MusicManager object that
    plays cool free music and will not duplicate on restart
    - a Lose collider at the bottom of the level
    - a Paddle that can move left and right based on the mouse, but is
    clamped to the width of the game screen
    - a Ball that starts attached to the paddle, and launches on mouseClick

---
+++ BrickBreaker/Scripts/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static bool instance = false;
+	
+	void Awake() {
+	
+		if (instance)
+		{
+			Destroy (gameObject);
+			print ("Duplicate music player self-destructing!");
+		}
+		else {
+			instance = true;
+			GameObject.DontDestroyOnLoad (gameObject);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ BrickBreaker/Scripts/Paddle.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	
+		/*
+		 this.transform.position.x = mousePos won't work
+		 The transform position must be changed as a whole vector3,
+		 since the individual x y z components can't be changed.
+		 Therefore, a temporary vector variable is created to change
+		 the paddle position. 
+		*/
+		
+		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
+		
+		// mousePos is given in pixels. First we must convert it to
+		// world units so it can be put in the transform component.
+		// We also want to clamp it so it doesn't leave the screen which is 16
+		// units wide. The paddle is 1 unit wide, so we use 0.5 and 15.5
+		// since the transform is based on the middle of the paddle.
+		
+		float mousePos = Mathf.Clamp (Input.mousePosition.x / Screen.width * 16, 0.5f, 15.5f);
+		paddlePos.x = mousePos;
+		
+		this.transform.position = paddlePos;
+	}
+}
