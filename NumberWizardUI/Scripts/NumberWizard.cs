623bed2d94bff69e78c7da017b7e2bf400f2bd10
cashWizard <jcstevens@utexas.edu>
2015-05-27 18:26:02 -0500

    Added NumberWizardUI
    This “game” demonstrates UI elements and scene changes using a
    LevelManager script. The underlying “game” is the same as NumberWizard,
    a number guessing game using binary search.

---
+++ NumberWizardUI/Scripts/NumberWizard.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+// This program is a number guessing game, using a "binary search" algorithm
+
+public class NumberWizard : MonoBehaviour {
+
+int min;
+int max;
+int guess;
+int maxGuessesAllowed = 10;
+
+public Text text;
+
+	// Use this for initialization
+	void Start () {
+		StartGame ();
+	}
+	
+	void StartGame() {
+		max = 1000;
+		min = 1;
+		guess = Random.Range (1,1001);
+		text.text = guess.ToString ();
+	
+	}
+	
+	void NextGuess() {
+		// Binary chop! Hi-yah!
+		guess = (max + min) / 2;
+		text.text = guess.ToString();
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		if (maxGuessesAllowed <= 0){
+			Application.LoadLevel("Win");
+		}
+	}
+	
+	public void GuessHigher(){
+		min = guess;
+		NextGuess ();
+	}
+	
+	public void GuessLower(){
+		max = guess;
+		NextGuess ();
+	}
+	
+}
